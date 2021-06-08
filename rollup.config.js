e5261e4a4bf09bf2ec1c8723bf7ade8c7561fecf
David Fisher <fisherds@rose-hulman.edu>
2021-06-08 10:16:09 -0400

    manual rollback attempt

--- rollup.config.js
+++ rollup.config.js
@@ -8,27 +8,24 @@ const babelOptions = JSON.parse(fs.readFileSync('./.babelrc'));
 export default {
   output: {
     format: 'umd',
-    globals: {
-      jquery: 'jQuery',
-      'popper.js': 'Popper',
-    },
+  },
+  globals: {
+    jquery: 'jQuery',
+    'popper.js': 'Popper',
   },
   external: ['jquery', 'popper.js'],
   plugins: [
-    babel(Object.assign(babelOptions, {
-      exclude: 'node_modules/**', // only transpile our source code
+    babel({
       externalHelpersWhitelist: [
         // Include only required helpers
         'defineProperties',
         'createClass',
         'inheritsLoose',
         'extends',
-        'objectSpread',
-        'defineProperty',
       ],
-    }) ),
+    }),
     resolve({
-      mainFields: ['module', 'main'], // Default: ['module', 'main']
+      module: true,
     }),
     cjs({
       include: ['node_modules/bootstrap/**', 'node_modules/jquery/**'],
